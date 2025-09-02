319a9a67d66789383f6df28ed15ff0d78dd1c65f
Jeff Gutierrez <jeffgutierrez0524@gmail.com>
2025-09-02 13:58:24 -0400

    Initial Commit

---
+++ Football/FootballApp.swift
@@ -0,0 +1,17 @@
+//
+//  FootballApp.swift
+//  Football
+//
+//  Created by Jeff Gutierrez on 9/2/25.
+//
+
+import SwiftUI
+
+@main
+struct FootballApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
